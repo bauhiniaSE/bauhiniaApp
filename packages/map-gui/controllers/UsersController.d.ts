c6b6273830867b7668e7c985ac8e73090d1d47bb
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-11-14 03:01:06 +0100

    feat: add template for gui

---
+++ packages/map-gui/controllers/UsersController.d.ts
@@ -0,0 +1,3 @@
+declare const usersRouter: import("express-serve-static-core").Router;
+export default usersRouter;
+//# sourceMappingURL=UsersController.d.ts.map
---
+++ packages/map-gui/models/User.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
+exports.default = User;
+//# sourceMappingURL=User.js.map
