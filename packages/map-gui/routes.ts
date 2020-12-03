4016a9f75b2554a24b753bcbb49d5f45ab062180
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-03 18:40:46 +0100

    Home controller added to routes.

--- packages/map-gui/routes.ts
+++ packages/map-gui/routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import usersRouter from './controllers/UsersController';
 import loginRouter from './controllers/LoginController';
 import registerRouter from './controllers/RegisterController';
+import homeRouter from './controllers/HomeController';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/login', loginRouter);
 routes.use('/register', registerRouter);
+routes.use('/', homeRouter);
 export default routes;
