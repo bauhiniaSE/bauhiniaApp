cab266cc6126a84352d2bf823b212cd3d2b13a7e
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-03 16:38:21 +0100

    Register and login views created (html + css). Controllers added.

--- packages/map-gui/routes.ts
+++ packages/map-gui/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import usersRouter from './controllers/UsersController';
+import loginRouter from './controllers/LoginController';
+import registerRouter from './controllers/RegisterController';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
-
+routes.use('/login', loginRouter);
+routes.use('/register', registerRouter);
 export default routes;
