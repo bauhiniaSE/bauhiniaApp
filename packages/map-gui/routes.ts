f78ef72a46057eab02d857ef86875d016fe5ebb3
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-04 10:34:19 +0100

    Grid for editor of the game

--- packages/map-gui/routes.ts
+++ packages/map-gui/routes.ts
@@ -7,6 +7,7 @@ import loginRouter from './controllers/LoginController';
 import registerRouter from './controllers/RegisterController';
 import startGameRouter from './controllers/StartGameController';
 import manageMapsRouter from './controllers/ManageMapsController';
+import editProjectRouter from './controllers/EditProjectController';
 
 const routes = Router();
 
@@ -16,6 +17,7 @@ routes.use('/', homeRouter);
 routes.use('/login', loginRouter);
 routes.use('/register', registerRouter);
 routes.use('/game', startGameRouter);
+routes.use('/game/edit', editProjectRouter);
 routes.use('/maps', manageMapsRouter);
 
 export default routes;
