b4d46e53870c4d8cce2e264c5da5ac521dad810f
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-10 14:32:58 +0100

    convertMapToPicture sketch for testing.

--- packages/save-system/src/SaveSystem.ts
+++ packages/save-system/src/SaveSystem.ts
@@ -1,10 +1,33 @@
+import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+
 export class SaveSystem {
   /**
      * convertMapToPicture
 map: any     */
   //map should have type Map
-  public convertMapToPicture(map: any): any {
-    return 'AAA';
+  public filter(url: any) {
+    return url.tagName !== 'i';
+  }
+  public convertMapToPicture(url: any): any {
+    //jesli nie dziala to wrzucic tu function filter i usunac this
+    htmlToImage.toSvg(url.getElementById('my-node'), { filter: this.filter }).then(function (dataUrl) {
+      var link = url.createElement('a');
+      link.download = 'test.jpeg';
+      link.href = dataUrl;
+      link.click();
+    });
+    /*var node = url.getElementById('my-node');
+    htmlToImage
+      .toPng(node)
+      .then(function (dataUrl) {
+        var img = url.createElement('img');
+        img.setAttribute('src', dataUrl);
+        url.body.appendChild(img);
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });*/
   }
   /**
    * saveMapToDatabase
