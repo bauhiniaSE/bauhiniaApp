55571c9fe641fab1168e2cac9a447d6228ffaddc
Maciej Szulc <224103@edu.p.lodz.pl>
2020-12-12 17:36:35 +0100

    simulation requires a map

--- packages/simulation-service/src/isimulation.ts
+++ packages/simulation-service/src/isimulation.ts
@@ -1,6 +1,8 @@
+import { MockMap } from './mockmap';
+
 export interface ISimulationService {
-  simulateFromScratch(): void;
-  simulateFromCache(): void;
+  simulateFromScratch(map: MockMap): void;
+  simulateFromCache(map: MockMap): void;
 
   getTemperature(x: number, y: number, altitude?: number): number;
 }
