7de10eaba4591e6a74046b8903d8e3ad6d4965f0
Mariusz <mariuszpisarski0@gmail.com>
2020-12-12 16:15:41 +0100

    fix: IMapManager without errors

--- packages/map-manager/src/MapManager.ts
+++ packages/map-manager/src/MapManager.ts
@@ -1,32 +1,35 @@
 class MapManager implements IMapManager {
-  listAllUserGames(userId: string): MyMap[] {
+  private readonly objectRepository: IObjectRepository;
+  private readonly mapRepository: IMapRepository;
+
+  public listAllUserGames(userId: string): MyMap[] {
     throw new Error('Method not implemented.');
   }
-  createNewGame(mapId: string): boolean {
+  public createNewGame(mapId: string): boolean {
     throw new Error('Method not implemented.');
   }
-  listAllMapsToPlay(): MyMap[] {
+  public listAllMapsToPlay(): MyMap[] {
     throw new Error('Method not implemented.');
   }
-  saveGame(userId: string, game: MyMap): void {
+  public saveGame(userId: string, game: MyMap): void {
     throw new Error('Method not implemented.');
   }
-  getObjectHeight(objectId: string): number {
+  public getObjectHeight(objectId: string): number {
     throw new Error('Method not implemented.');
   }
-  getObjectWidth(objectId: string): number {
+  public getObjectWidth(objectId: string): number {
     throw new Error('Method not implemented.');
   }
-  getObjectLength(objectId: string): number {
+  public getObjectLength(objectId: string): number {
     throw new Error('Method not implemented.');
   }
-  getObjectPrice(objectId: string): number {
+  public getObjectPrice(objectId: string): number {
     throw new Error('Method not implemented.');
   }
-  getObjectType(objectId: string): string {
+  public getObjectType(objectId: string): string {
     throw new Error('Method not implemented.');
   }
-  startGame(gameId: string): void {
+  public startGame(gameId: string): void {
     throw new Error('Method not implemented.');
   }
 }
